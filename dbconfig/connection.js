6ad641f3250d2a2086824ee09b84f41b18da9039
rahul <rahulsidharthan18@gmail.com>
2023-05-19 21:36:15 +0530

    node modules removed 01

--- dbconfig/connection.js
+++ dbconfig/connection.js
@@ -4,7 +4,7 @@ const state = {
 }
 
 module.exports.connect = function (done) {
-    const url = "mongodb://127.0.0.1:27017"
+    const url = "mongodb+srv://rahulsidharthan18:5LBhjaNiGrO818eN@cluster0.2l5wqgs.mongodb.net/?retryWrites=true&w=majority"
     const dbname = 'userdata'
 
     MongoClient.connect(url, (err, data) => {
